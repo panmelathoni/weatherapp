b4d341d67926da209c82e1b3c27db7063592cbe8
Panmela Thoni <panmelathoni@gmail.com>
2021-11-11 16:35:29 +0100

    api calls and design first part

--- src/app.js
+++ src/app.js
@@ -1,7 +1,67 @@
+// *** API geolocation call ***
+
+navigator.geolocation.getCurrentPosition(showCurrentPosition);
+
+function showCurrentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "d501ce4c88c1a995f848c610a71cfad4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
+
+  axios.get(apiUrl).then(function (response) {
+    let currentCityName = document.querySelector("#heading");
+    currentCityName.innerHTML = response.data.name;
+
+    let temperature = document.querySelector("#temperature-change");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+
+    let weatherDescription = document.querySelector(".weather-text");
+    weatherDescription.innerHTML = response.data.weather[0].main;
+
+    let tempMin = document.querySelector(".temp-min");
+    tempMin.innerHTML = Math.round(response.data.main.temp_min);
+
+    let tempMax = document.querySelector(".temp-max");
+    tempMax.innerHTML = Math.round(response.data.main.temp_max);
+
+    let feelsLike = document.querySelector(".feels-like");
+    feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  });
+}
+
+let btnCurrentLocation = document.querySelector(".location");
+btnCurrentLocation.addEventListener("click", showPosition);
+
+function showPosition() {
+  showCurrentPosition();
+}
+
 function searchLocaltion(e) {
   e.preventDefault();
+
+  let apiKey = "d501ce4c88c1a995f848c610a71cfad4";
+
   let inputCity = document.querySelector("#inputCity");
-  heading.textContent = inputCity.value;
+  console.log(inputCity.value);
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=${apiKey}&&units=metric`;
+  heading.innerHTML = inputCity.value;
+
+  axios.get(apiUrlCity).then(function (response) {
+    let temperature = document.querySelector("#temperature-change");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+
+    let weatherDescription = document.querySelector(".weather-text");
+    weatherDescription.innerHTML = response.data.weather[0].main;
+
+    let tempMin = document.querySelector(".temp-min");
+    tempMin.innerHTML = Math.round(response.data.main.temp_min);
+
+    let tempMax = document.querySelector(".temp-max");
+    tempMax.innerHTML = Math.round(response.data.main.temp_max);
+
+    let feelsLike = document.querySelector(".feels-like");
+    feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  });
 }
 let searchCity = document.querySelector("#form");
 searchCity.addEventListener("submit", searchLocaltion);
